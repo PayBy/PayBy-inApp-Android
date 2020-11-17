cffbabc95daa4533220e2f9a960f7220c081684a
zhouqinlong <qinlong.zhou@payby.com>
2020-11-17 14:57:43 +0800

    feat:增加IAP签约和下单回调

--- IAPSample/app/src/main/java/com/payby/android/payment/iap/sample/MainActivity.java
+++ IAPSample/app/src/main/java/com/payby/android/payment/iap/sample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.payby.android.payment.iap.sample;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -9,9 +10,15 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.payby.android.payment.iap.view.x.OnPayResultListener;
-import com.payby.android.payment.iap.view.x.PayTask;
-import com.payby.android.payment.iap.view.x.PbManager;
+import com.payby.android.iap.view.Environment;
+import com.payby.android.iap.view.OnOrderFailCallback;
+import com.payby.android.iap.view.OnOrderSuccessCallback;
+import com.payby.android.iap.view.OnPayResultListener;
+import com.payby.android.iap.view.PayTask;
+import com.payby.android.iap.view.PbManager;
+
+import java.nio.charset.StandardCharsets;
+
 
 public class MainActivity extends AppCompatActivity implements OnPayResultListener {
   EditText et_sign, et_token, et_id, et_deviceId, et_app_id;
@@ -22,8 +29,7 @@ public class MainActivity extends AppCompatActivity implements OnPayResultListen
   private String mSign;
   private String mIapDeviceId;
   private String mAppId;
-  private boolean isDev = true; //生产环境还是开发环境
-  private String privateKay = "113";  //当前商户的私钥，
+  private String keyDev = "";//当前商户的私钥，
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements OnPayResultListen
     manager = PbManager.getInstance(this);
 
     //Step2:generate iapDeviceId
-    String iapDeviceID = manager.getIAPDeviceID();
+    String iapDeviceID = manager.getIAPDeviceID(this);
     et_deviceId.setText(iapDeviceID);
 
     //Step3:register the payment callback
@@ -59,6 +65,37 @@ public class MainActivity extends AppCompatActivity implements OnPayResultListen
   }
 
   private void startPay() {
+    //
+    manager.payWithOrderCallback(this);
+  }
+
+  @Override
+  public void onGetPayState(String result) {
+    if (TextUtils.equals(result, "SUCCESS")) {
+      //成功，已经收款，交易结束
+    } else if (TextUtils.equals(result, "PAID")) {
+      // 已经付款
+    } else if (TextUtils.equals(result, "PAYING")) {
+      // 正在处理
+    } else if (TextUtils.equals(result, "FAIL")) {
+      // 支付失败
+    } else {
+      // 其他未知错误
+    }
+  }
+
+  @Override
+  public void onGetProtocolState(String protocolState) {
+    //PROTOCOL-SUCCESS,PROTOCOL-FAIL
+    if (TextUtils.equals(protocolState, "PROTOCOL-SUCCESS")) {
+      //success......
+    } else if (TextUtils.equals(protocolState, "PROTOCOL-FAIL")) {
+      //fail.....
+    }
+  }
+
+  @Override
+  public void onOrder(OnOrderSuccessCallback onOrderSuccessCallback, OnOrderFailCallback onOrderFailCallback) {
     mToken = et_token.getText().toString().trim();
     mPartnerId = et_id.getText().toString().trim();
     mIapDeviceId = et_deviceId.getText().toString().trim();
@@ -79,22 +116,28 @@ public class MainActivity extends AppCompatActivity implements OnPayResultListen
 //        RsaUtils.sign(
 //            signString, StandardCharsets.UTF_8, RsaUtils.getPrivateKey(privateKay)));
 
-    PayTask task = PayTask.with(mToken, mIapDeviceId, mPartnerId, mSign, mAppId);
-    manager.pay(task, isDev);
-  }
+    if (!TextUtils.isEmpty(mToken)) {
+      String signString = "iapAppId=" + mAppId + "&iapDeviceId=" + mIapDeviceId + "&iapPartnerId=" + mPartnerId + "&token=" + mToken;
 
-  @Override
-  public void onGetPayState(String result) {
-    if (TextUtils.equals(result, "SUCCESS")) {
-      //成功，已经收款，交易结束
-    } else if (TextUtils.equals(result, "PAID")) {
-      // 已经付款
-    } else if (TextUtils.equals(result, "PAYING")) {
-      // 正在处理
-    } else if (TextUtils.equals(result, "FAIL")) {
-      // 支付失败
+      String sign = Base64.encode(
+
+          RsaUtils.sign(
+              signString, StandardCharsets.UTF_8, RsaUtils.getPrivateKey(keyDev)));
+      PayTask task = PayTask.with(mToken, mIapDeviceId, mPartnerId, sign, mAppId);
+      new Handler().postDelayed(new Runnable() {
+        @Override
+        public void run() {
+          onOrderSuccessCallback.onSuccess(task, Environment.DEV);
+        }
+      }, 3000);
     } else {
-      // 其他未知错误
+      new Handler().postDelayed(new Runnable() {
+        @Override
+        public void run() {
+          onOrderFailCallback.onFail();
+        }
+      }, 4000);
     }
   }
+
 }
